3a9fbc5294c8cae2c204d0b7b8945ba89ad20501
thelrner <peteryhua@gmail.com>
2015-11-29 20:57:10 -0500

    Finalizes basic texting functionality.

--- texter.rb
+++ texter.rb
@@ -1,9 +1,10 @@
 # https://www.twilio.com/docs/tutorials/walkthrough/server-notifications/ruby/rails#0
 
 require 'twilio-ruby'
+require 'yaml'
 
 class Texter
-  attr_reader :contacts, :sender
+  attr_reader :client, :contacts, :default_from
 
   def initialize
     environment = YAML.load_file('application.yml')
@@ -15,14 +16,15 @@ class Texter
     @default_from = @contacts['me']
   end
 
-  def send_to(name, message = 'hello!')
+  def send_to(name, message = 'hello!', media_url = nil)
     recipient_num = self.contacts[name.downcase]
     sender_num = self.default_from
     send_message(sender_num, recipient_num, message, media_url)
+    puts "Message sent to #{name}"
   end
 
   # set up a client to talk to the Twilio REST API
-  def send_message(from_num, to_num, body, media_url = nil)
+  def send_message(from_num, to_num, body, media_url)
     message_hash = {
       from: from_num,
       to: to_num,
@@ -31,7 +33,9 @@ class Texter
     message_hash[:media_url] = media_url if media_url
 
     self.client.account.messages.create(message_hash)
+    puts "Sent message to #{to_num}"
   end
 end
 
 texter = Texter.new
+# texter.send_to('ly', 'wanna go brush teef?')
